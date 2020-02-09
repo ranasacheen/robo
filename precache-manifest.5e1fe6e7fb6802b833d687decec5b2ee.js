ce27d92890fd9b8e15820c2344ad5d65812a4d2e
ranasacheen <sacheenrana@gmail.com>
2020-02-09 21:58:47 +0545

    Updates

---
+++ precache-manifest.5e1fe6e7fb6802b833d687decec5b2ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b39a4c8cd0e91bd8391d17ce44ccb30",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "ae1d91827313cd095918",
+    "url": "/robo/static/css/main.c6ae8980.chunk.css"
+  },
+  {
+    "revision": "bd64c4b46ee60c46d6ef",
+    "url": "/robo/static/js/2.369f7176.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robo/static/js/2.369f7176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1d91827313cd095918",
+    "url": "/robo/static/js/main.0b3718e0.chunk.js"
+  },
+  {
+    "revision": "9b8eb86b7be7fd3e1625",
+    "url": "/robo/static/js/runtime-main.d42d5b2f.js"
+  }
+]);
